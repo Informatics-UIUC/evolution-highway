242207e982b06d1a4e2cedd64c3d29c2b595eb20
Boris Capitanu <capitanu@illinois.edu>
2010-10-09 04:23:02 +0000

    EH-1: fixed issue with situation when the SIGN column contained NULL values

--- EvolutionHighwayWidgets/Converters/SignConverter.cs
+++ EvolutionHighwayWidgets/Converters/SignConverter.cs
@@ -18,16 +18,16 @@ namespace EvolutionHighwayWidgets.Converters
 
         #region Sign attached property definition
         public static readonly DependencyProperty SignProperty =
-            DependencyProperty.RegisterAttached("Sign", typeof(int), typeof(SignConverter), null);
+            DependencyProperty.RegisterAttached("Sign", typeof(int?), typeof(SignConverter), null);
 
-        public static void SetSign(DependencyObject obj, int sign)
+        public static void SetSign(DependencyObject obj, int? sign)
         {
             obj.SetValue(SignProperty, sign);
         }
 
-        public static int GetSign(DependencyObject obj)
+        public static int? GetSign(DependencyObject obj)
         {
-            return (int) obj.GetValue(SignProperty);
+            return (int?) obj.GetValue(SignProperty);
         }
         #endregion
 
@@ -38,16 +38,19 @@ namespace EvolutionHighwayWidgets.Converters
 
         public object Convert(object o, Type targetType, object parameter, CultureInfo culture)
         {
-            if (o.GetType() != typeof(int))
+            if (o != null && o.GetType() != typeof(int))
                 throw new ArgumentException("Expected integer type for conversion");
-            
-            var sign = (int) o;
-            var convertedValue = (from DependencyObject depObj in Values 
-                                  let depObjSign = GetSign(depObj) 
-                                  where sign == depObjSign 
-                                  select depObj).FirstOrDefault();
 
-            if (convertedValue != null && !targetType.IsAssignableFrom(convertedValue.GetType()))
+            var sign = (int?) o;
+            var convertedValue = (from DependencyObject depObj in Values
+                                  let depObjSign = GetSign(depObj)
+                                  where sign == depObjSign
+                                  select depObj).FirstOrDefault() ?? (from DependencyObject depObj in Values
+                                                                      let depObjSign = GetSign(depObj)
+                                                                      where depObjSign == null
+                                                                      select depObj).FirstOrDefault();
+
+            if (!targetType.IsAssignableFrom(convertedValue.GetType()))
                 throw new InvalidCastException(
                     String.Format("Cannot cast {0} into {1} - Values must be of type {1}", convertedValue.GetType(), targetType));
 
